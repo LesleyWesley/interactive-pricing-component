bdb1832c2034ab5c713a39979be6255bf6fb21e2
lesley_w <lrwesley92@gmail.com>
2022-01-12 15:35:34 -0600

    Added variables for JavaScript

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//======= Variables ===========
+
+//Selects number of page views span
+
+const pageviews = document.querySelector(".view-count");
+
+//Selects price per month span
+
+const price = document.querySelector(".price");
+
+//Selects the range Input
+
+const range = document.querySelector(".real-range");
+
+//Selects checkbox for yearly billing
+
+const checkbox = document.querySelector(".checkbox");
+
+//Selects visuals for toggle button, specifically the outer div
+
+const switchButton = document.querySelector(".switch-holder");
