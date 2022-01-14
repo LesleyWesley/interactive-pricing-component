a3737c705f809a5568de0886936150df16af97fa
lesley_w <lrwesley92@gmail.com>
2022-01-13 21:13:14 -0600

    Added function to remove range slider fill

--- js/script.js
+++ js/script.js
@@ -19,3 +19,36 @@ const checkbox = document.querySelector(".checkbox");
 //Selects visuals for toggle button, specifically the outer div
 
 const switchButton = document.querySelector(".switch-holder");
+
+let yearlyBilling = false;
+
+//=========================================================================
+
+//Adds event listener for checkbox
+
+checkbox.addEventListener("click", function () {
+  if (checkbox.checked == true) {
+    switchButton.classList.add("active");
+    yearlyBilling = true;
+    console.log(yearlyBilling);
+  } else {
+    switchButton.classList.remove("active");
+    yearlyBilling = false;
+    console.log(yearlyBilling);
+  }
+})
+
+//======================================================================
+
+//Removes fill from range input
+
+  const removeFill = function() {
+    range.classList.remove("percent-25");
+    range.classList.remove("percent-50");
+    range.classList.remove("percent-75");
+    range.classList.remove("percent-100");
+  }
+
+//=======================================================================
+
+//Adds event listener for range Input
