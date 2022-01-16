fc60ea844f8417b75377b753fcf5d1e6a65895a5
lesley_w <lrwesley92@gmail.com>
2022-01-15 18:09:15 -0600

    Added ability to update pageviews based and calculate total price based on range value

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const pageviews = document.querySelector(".view-count");
 //Selects price per month span
 
 const price = document.querySelector(".price");
-let priceValue;
+const priceNumeral = document.querySelector(".calculated-price");
 
 //Selects the range Input
 
@@ -24,14 +24,36 @@ const switchButton = document.querySelector(".switch-holder");
 
 let yearlyBilling = false;
 
+let calculatedPrice = 16;
+
 //========================================================================
 
 //Main Function
 
 range.addEventListener("input", function () {
+  updateViews();
   removeFill();
   addFill();
+  calculatePrice();
 })
+
+//=========================================================================
+
+//Updates number of pagesviews based on range input
+
+const updateViews = function () {
+  if (range.value == 1) {
+    pageviews.innerText = "10K";
+  } else if (range.value == 2) {
+    pageviews.innerText = "50K";
+  } else if (range.value == 3) {
+    pageviews.innerText = "100K";
+  } else if (range.value == 4) {
+    pageviews.innerText = "500K";
+  } else if (range.value == 5) {
+    pageviews.innerText = "1M";
+  }
+}
 
 
 //=========================================================================
@@ -42,11 +64,13 @@ checkbox.addEventListener("click", function () {
   if (checkbox.checked == true) {
     switchButton.classList.add("active");
     yearlyBilling = true;
+    calculatePrice()
     console.log(yearlyBilling);
   } else {
     switchButton.classList.remove("active");
     yearlyBilling = false;
     console.log(yearlyBilling);
+    calculatePrice();
   }
 })
 
@@ -79,3 +103,24 @@ const addFill = function () {
 }
 
 //======================================================================
+
+const calculatePrice = function () {
+  if (range.value == 1) {
+    calculatedPrice = 8;
+  } else if (range.value == 2) {
+    calculatedPrice = 12;
+  } else if (range.value == 3) {
+    calculatedPrice = 16;
+  } else if (range.value == 4) {
+    calculatedPrice = 24;
+  } else if (range.value == 5) {
+    calculatedPrice = 36;
+  }
+
+  if (yearlyBilling == true) {
+    calculatedPrice = calculatedPrice * .75;
+  }
+
+  priceNumeral.innerText = calculatedPrice;
+
+}
